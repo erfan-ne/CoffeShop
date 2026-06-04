0f5700b42aa532533d8b165882f1852b581c4e0c
erfan-ne <erfan.neshati@gmail.com>
2026-06-04 16:46:30 +0330

    add slider template

---
+++ components/templates/Home/Slider.js
@@ -0,0 +1,49 @@
+import React from "react";
+import styles from "@/styles/slider.module.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+
+function Slider() {
+  return (
+    <Swiper
+      loop={true}
+      navigation={true}
+      modules={[Navigation]}
+      className={styles.swiper}
+    >
+      <SwiperSlide
+        className={styles.swiper_slide}
+        style={{ backgroundImage: 'url("/images/carousel-1.jpg")' }}
+      >
+        <div
+          className={`${styles.slider_caption} d-flex flex-column align-items-center justify-content-center`}
+        >
+          <h2 className="text-primary font-weight-medium m-0">
+            We Have Been Serving
+          </h2>
+          <h1 className="display-1 text-white m-0">COFFEE</h1>
+          <h2 className="text-white m-0">* SINCE 1950 *</h2>
+        </div>
+      </SwiperSlide>
+
+      <SwiperSlide
+        className={styles.swiper_slide}
+        style={{ backgroundImage: 'url("/images/carousel-2.jpg")' }}
+      >
+        <div
+          className={`${styles.slider_caption} d-flex flex-column align-items-center justify-content-center`}
+        >
+          <h2 className="text-primary font-weight-medium m-0">
+            We Have Been Serving
+          </h2>
+          <h1 className="display-1 text-white m-0">COFFEE</h1>
+          <h2 className="text-white m-0">* SINCE 1950 *</h2>
+        </div>
+      </SwiperSlide>
+    </Swiper>
+  );
+}
+
+export default Slider;
