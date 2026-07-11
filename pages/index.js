1a1443a146e1864bf9c18db53fe23ae3a34fcd7d
erfan-ne <erfan.neshati@gmail.com>
2026-07-11 15:33:30 +0330

    add Reservation in HomePage

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import About from "@/components/templates/Home/About";
 import Services from "@/components/templates/Home/Services";
 import Offer from "@/components/templates/Home/Offer";
 import Menu from "@/components/templates/Home/Menu";
+import Reservation from "@/components/templates/Home/Reservation";
 
 function Home() {
   return (
@@ -13,6 +14,7 @@ function Home() {
       <Services/>
       <Offer/>
       <Menu/>
+      <Reservation/>
     </>
   );
 }
