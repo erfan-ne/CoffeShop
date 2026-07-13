52112d1c23fd711624456400d9e83609c51eb66e
erfan-ne <erfan.neshati@gmail.com>
2026-07-13 18:42:03 +0330

    update project in offer module

--- components/templates/Home/Offer.js
+++ components/templates/Home/Offer.js
@@ -1,23 +1,57 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function Offer() {
+  const [email, setEmail] = useState("");
+
+  const addEmail = async (e) => {
+    e.preventDefault();
+
+    const res = await fetch("http://localhost:3001/newsLetters", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email }),
+    });
+
+    if (res.status === 201) {
+      setEmail("");
+      alert("your email is added");
+    }
+  };
+
   return (
     <div className="offer container-fluid my-5 py-5 text-center position-relative overlay-top overlay-bottom">
-        <div className="container py-5">
-            <h1 className="display-3 text-primary mt-3">50% OFF</h1>
-            <h1 className="text-white mb-3">Sunday Special Offer</h1>
-            <h4 className="text-white font-weight-normal mb-4 pb-3">Only for Sunday from 1st Jan to 30th Jan 2045</h4>
-            <form className="form-inline justify-content-center mb-4">
-                <div className="input-group">
-                    <input type="text" className="form-control p-4" placeholder="Your Email" style={{height: "60px"}} />
-                    <div className="input-group-append">
-                        <button className="btn btn-primary font-weight-bold px-4" type="submit">Sign Up</button>
-                    </div>
-                </div>
-            </form>
-        </div>
+      <div className="container py-5">
+        <h1 className="display-3 text-primary mt-3">50% OFF</h1>
+        <h1 className="text-white mb-3">Sunday Special Offer</h1>
+        <h4 className="text-white font-weight-normal mb-4 pb-3">
+          Only for Sunday from 1st Jan to 30th Jan 2045
+        </h4>
+        <form className="form-inline justify-content-center mb-4">
+          <div className="input-group">
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              type="text"
+              className="form-control p-4"
+              placeholder="Your Email"
+              style={{ height: "60px" }}
+              value={email}
+            />
+            <div className="input-group-append">
+              <button
+                onClick={addEmail}
+                className="btn btn-primary font-weight-bold px-4"
+                type="submit"
+              >
+                JOIN
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Offer
+export default Offer;
--- pages/index.js
+++ pages/index.js
@@ -7,30 +7,31 @@ import Menu from "@/components/templates/Home/Menu";
 import Reservation from "@/components/templates/Home/Reservation";
 import Testimonial from "@/components/templates/Home/Testimonial";
 
-function Home({services}) {
+function Home({ data }) {
   return (
-    <>    
+    <>
       <Slider />
       <About />
-      <Services Services={services}/>
-      <Offer/>
-      <Menu/>
-      <Reservation/>
+      <Services Services={data.services} />
+      <Offer />
+      <Menu />
+      <Reservation />
       <Testimonial />
     </>
   );
 }
 
-export async function getStaticProps(){
-
+export async function getStaticProps() {
   const res = await fetch("http://localhost:3001/services");
-  const data = await res.json()
+  const services = await res.json();
 
-  return{
-    props:{
-      services: data
-    }
-  }
+  return {
+    props: {
+      data:{
+        services
+      }
+    },
+  };
 }
 
 export default Home;
