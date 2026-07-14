df040193d1ac29b5d920944f4f6a4183d2f9fb20
erfan-ne <erfan.neshati@gmail.com>
2026-07-14 11:04:08 +0330

    add images

--- components/templates/Home/Menu.js
+++ components/templates/Home/Menu.js
@@ -19,38 +19,17 @@ function Menu({ Menus }) {
           <div className="col-lg-6">
             <h1 className="mb-5">Hot Coffee</h1>
 
-            {Menus.map((menu) => {
+            {Menus.filter(menu=>menu.type === "hot").slice(0, 3).map((menu) => {
               return (
                 <MenuItem
                   key={menu.id}
-                  img="images/menu-1.jpg"
-                  price="$5"
-                  title="Black Coffee"
-                  text="Sit lorem ipsum et diam elitr est dolor sed duo guberg sea et et lorem dolor"
+                  img={menu.img}
+                  price={`$${menu.price}`}
+                  title={menu.title}
+                  text={menu.desc}
                 />
               );
             })}
-
-            <MenuItem
-              img="images/menu-1.jpg"
-              price="$5"
-              title="Black Coffee"
-              text="Sit lorem ipsum et diam elitr est dolor sed duo guberg sea et et lorem dolor"
-            />
-
-            <MenuItem
-              img="images/menu-2.jpg"
-              price="$7"
-              title="Chocolete Coffee"
-              text="Sit lorem ipsum et diam elitr est dolor sed duo guberg sea et et lorem dolor"
-            />
-
-            <MenuItem
-              img="images/menu-3.jpg"
-              price="$9"
-              title="Black Coffee"
-              text="Sit lorem ipsum et diam elitr est dolor sed duo guberg sea et et lorem dolor"
-            />
           </div>
 
           <div className="col-lg-6">
