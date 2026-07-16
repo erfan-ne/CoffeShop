2084f35da59041ca2ed0ba1bf7dfeb6a55370c99
erfan-ne <erfan.neshati@gmail.com>
2026-07-16 19:27:34 +0330

    update searchBar

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,12 +1,29 @@
 import PageHeader from '@/components/modules/PageHeader/PageHeader'
+import Resault from '@/components/templates/Search/Resault'
 import React from 'react'
 
-function SearchPage() {
+function SearchPage({resault}) {
   return (
     <>
     <PageHeader route="Search" />
+    <Resault Search={resault} />
     </>
   )
+}
+
+export async function getServerSideProps(contex) {
+  const {query} = contex
+
+  const res = await fetch("http://localhost:3001/menu")
+  const data = await res.json()
+
+  const searchResault = data.filter(item => item.type.toLowerCase().includes(query.q.toLowerCase()) || item.title.toLowerCase().includes(query.q.toLowerCase()))
+
+  return {
+    props : {
+      resault: searchResault
+    }
+  }
 }
 
 export default SearchPage
