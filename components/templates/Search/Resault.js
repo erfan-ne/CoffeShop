a9736bf38d671204ae227162e82c056380309cce
erfan-ne <erfan.neshati@gmail.com>
2026-07-16 19:28:05 +0330

    create searchResault template

---
+++ components/templates/Search/Resault.js
@@ -0,0 +1,39 @@
+import MenuItem from "@/components/modules/Menu/MenuItem";
+import React from "react";
+
+function Resault({ Search }) {
+  return (
+    <>
+    {console.log(Search)}
+      {Search.length ? (
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6">
+              <h1 className="mb-5" style={{ textAlign: "center" }}>
+                Hot Coffee
+              </h1>
+
+              {Search.filter((menu) => menu.type === "hot").map((menu) => {
+              return <MenuItem key={menu.id} {...menu} />;
+              })}
+            </div>
+
+            <div className="col-lg-6">
+              <h1 className="mb-5" style={{ textAlign: "center" }}>
+                Cold Coffee
+              </h1>
+
+              {Search.filter((menu) => menu.type === "cold").map((menu) => {
+              return <MenuItem key={menu.id} {...menu} />;
+              })}
+            </div>
+          </div>
+        </div>
+      ):(
+        <h3 style={{textAlign:"center"}}>The product you are looking for was not found!</h3>
+      )}
+    </>
+  );
+}
+
+export default Resault;
