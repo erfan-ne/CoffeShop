ae7674725203f7c174ddd009cfaf29c2ea2c70f4
erfan-ne <erfan.neshati@gmail.com>
2026-07-16 16:47:32 +0330

    create searchBar in Navbar

--- components/modules/Navbar/Navbar.js
+++ components/modules/Navbar/Navbar.js
@@ -1,17 +1,50 @@
-import React from 'react'
-import styles from "@/styles/navbar.module.css"
+import React, { useState } from "react";
+import styles from "@/styles/navbar.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import * as Icons from "@fortawesome/free-solid-svg-icons";
+import { useRouter } from "next/router";
 
 function Navbar() {
+  const route = useRouter()
+  const [search, setSearch] = useState("");
+
+  const searchHandler = () => {
+    //code
+  };
+
+  const searchHandlerWithEnter = () => {
+    //code
+  };
+
   return (
     <div className={`container-fluid p-0 ${styles.nav_bar}`}>
       <nav
         className={`${styles.navbar} ${styles.navbar_expand_lg} bg-none navbar-dark py-3`}
       >
-        <a href="/" className={`${styles.navbar_brand} px-lg-4 m-0`}>
-          <h1 className="m-0 display-4 text-uppercase text-white">Next-Coffee</h1>
-        </a>
+        <div className="d-flex align-items-center position-relative">
+          <a href="/" className={`${styles.navbar_brand} px-lg-4 m-0`}>
+            <h1 className="m-0 display-4 text-uppercase text-white">
+              Next-Coffee
+            </h1>
+          </a>
 
-        
+          <div style={{ marginLeft: "1rem" }}>
+            <input
+              value={search}
+              onChange={(event) => setSearch(event.target.value)}
+              onKeyUp={searchHandlerWithEnter}
+              type="text"
+              className={styles.search_input}
+              placeholder="Search..."
+            />
+            <FontAwesomeIcon
+              onClick={searchHandler}
+              className={styles.search_icon}
+              icon={Icons.faSearch}
+            />
+          </div>
+        </div>
+
         <div
           className={`collapse ${styles.navbar_collapse} justify-content-between`}
           id="navbarCollapse"
@@ -35,12 +68,14 @@ function Navbar() {
             <div className={`${styles.dropdown}`}>
               <a
                 className={`${styles.nav_link} ${styles.dropdown_toggle}`}
-                style={{cursor:"pointer"}}
+                style={{ cursor: "pointer" }}
                 data-toggle="dropdown"
               >
                 Pages
               </a>
-              <div className={`${styles.dropdown_menu} ${styles.text_capitalize}`}>
+              <div
+                className={`${styles.dropdown_menu} ${styles.text_capitalize}`}
+              >
                 <a href="/reservation" className={`${styles.dropdown_item}`}>
                   Reservation
                 </a>
@@ -56,7 +91,7 @@ function Navbar() {
         </div>
       </nav>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
